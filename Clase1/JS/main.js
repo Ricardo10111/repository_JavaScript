750d5869fac51c1bcfe873962d0da700438d983a
Ricardo Lopez <albelop1m@gmail.com>
2024-02-22 19:47:15 -0600

    feat: create Clase 1 and Clase 2 Files

---
+++ Clase1/JS/main.js
@@ -0,0 +1,14 @@
+let comidaUno = 0;
+let comidaDos = 0; 
+
+const option = prompt('Por favor ingresa una opcion');
+
+if (option === 1 ) {
+    console.log ('Hola como estas? ')
+
+}
+
+
+// tipos de Datos
+
+
---
+++ Clase2/test/index.js
@@ -0,0 +1,4 @@
+const variable = 5; 
+const b = [1, 2, 3, 5];
+
+return 'hola';
